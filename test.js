c4043f92dedc50cf5d5f733e02c5402684d4a382
Daniel <49683346+0tii@users.noreply.github.com>
2022-06-27 11:33:42 +0200

    remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const { html2pdf, base64ToPdf } = require('./index.js');
-const { html } = require('./html.js');
-
-async function test() {
-    const b64 = await html2pdf(html, { avoidTableRowBreak: true, marginTop: 10, repeatTableHeader: false, landscape: true }); //path must exist
-    base64ToPdf(b64, './test.pdf');
-}
-
-test();
